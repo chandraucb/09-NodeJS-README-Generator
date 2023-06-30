3f5e49f6c3c3b0efe1f60d6b86a7d0da43b63a32
Chandrasekar Mohan <chandrasekar.mohan@gmail.com>
2023-06-30 01:11:12 -0700

    Added logic to prompt questions and create README

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # 09-NodeJS-README-Generator
-
-
+  
--- index.js
+++ index.js
@@ -1,13 +1,63 @@
-// TODO: Include packages needed for this application
+//imports
+const inquirer = require('inquirer')
+const fs = require('fs')
 
-// TODO: Create an array of questions for user input
-const questions = [];
+//README Questions
+const questions = [{
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?',
+  },
+  {
+    type: 'input',
+    name: 'location',
+    message: 'Where are you from?',
+  },
+  {
+    type: 'input',
+    name: 'hobby',
+    message: 'What is your favorite hobby?',
+  },
+  {
+    type: 'input',
+    name: 'food',
+    message: 'What is your favorite food?',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub Username',
+  },
+  {
+    type: 'input',
+    name: 'linkedin',
+    message: 'Enter your LinkedIn URL.',
+  }];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//Function to show user prompts
+const promptUser = () => {
+    return inquirer.prompt(questions)
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// function to write README file
+//function writeToFile(fileName, data) {}
+const { writeFile } = fs.promises;
+
+
+// function to generate README content
+const generateReadme = ({ name, location, github, linkedin }) =>
+  `${name}
+  `;
+
+// Function to initialize app
+function init() {
+    promptUser()
+    .then((answers) => {
+        console.log(answers)
+        writeFile('README.md', generateReadme(answers))
+    })
+    .then(()=> console.log('Read me file creatted successfully!'))
+}
 
 // Function call to initialize app
 init();
